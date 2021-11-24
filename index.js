68a41c4698c34b6ad8ad8ff7f2125204ba0abd3d
a-kontopoulos <a.kontopoulos@gmail.com>
2021-11-24 09:27:54 -0600

    fith commit

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const corsOptions ={
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions));
-
 app.use(express.json())
+app.use(express.static('build'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
